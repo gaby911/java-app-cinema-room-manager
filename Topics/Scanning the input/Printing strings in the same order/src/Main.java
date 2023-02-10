8b884e1cb17ff602ae376b46ae5d4735273c770e
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-10 08:14:25 +0200

    Printing strings in the same order

---
+++ Topics/Scanning the input/Printing strings in the same order/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String w1 = scanner.next();
+        String w2 = scanner.next();
+        String w3 = scanner.next();
+        String w4 = scanner.next();
+
+        System.out.println(w1 +"\n" +w2+"\n"+w3+"\n"+w4);
+    }
+}
