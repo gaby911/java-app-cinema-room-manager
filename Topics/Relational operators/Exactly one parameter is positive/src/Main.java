53d926f21a01355b77cc106269624322a392f5f2
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-11 12:43:29 +0200

    Exactly one parameter is positive

---
+++ Topics/Relational operators/Exactly one parameter is positive/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        /*
+        Exactly one parameter is positive
+        Write a program that reads three integer numbers and prints true if exactly ONE number is positive (i.e. > 0).
+
+        Otherwise, it should print false.
+
+
+        Sample Input:
+        1 1 1
+
+        Sample Output:
+        false
+
+
+        Sample Input:
+        1 0 -1
+
+        Sample Output:
+        true
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int thirdNumber = scanner.nextInt();
+
+        if (firstNumber > 0 && secondNumber <= 0 && thirdNumber <= 0) System.out.println(true);
+        else if (secondNumber > 0 && firstNumber <= 0 && thirdNumber <= 0) System.out.println(true);
+        else if (thirdNumber > 0 && firstNumber <= 0 && secondNumber <= 0) System.out.println(true);
+        else System.out.println(false);
+
+    }
+}
