074e729b1b6eea2ce41d56a4b86982b0b7d6b390
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-11 13:29:54 +0200

    Stage 2/5: Sold!

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,16 +1,35 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:\n" +
-                "  1 2 3 4 5 6 7 8\n" +
-                "1 S S S S S S S S\n" +
-                "2 S S S S S S S S\n" +
-                "3 S S S S S S S S\n" +
-                "4 S S S S S S S S\n" +
-                "5 S S S S S S S S\n" +
-                "6 S S S S S S S S\n" +
-                "7 S S S S S S S S");
+        enterNumberOfRowsAndSeatsAndCalculateProfit();
+
     }
+
+    static void enterNumberOfRowsAndSeatsAndCalculateProfit() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+
+        System.out.print("Enter the number of seats in each row: ");
+        int seatsPerRow = input.nextInt();
+
+        int totalSeats = rows * seatsPerRow;
+        int profit = 0;
+        if (totalSeats <= 60) {
+            profit = totalSeats * 10;
+        } else {
+            int frontRows = rows / 2;
+            int backRows = rows - frontRows;
+            profit = frontRows * seatsPerRow * 10 + backRows * seatsPerRow * 8;
+        }
+
+        System.out.println("Total income: $" + profit);
+
+    }
+
 }
