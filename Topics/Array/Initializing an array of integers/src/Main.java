22e14eb1d591310e1fa9660ad7ba335b610e565e
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-12 17:13:43 +0200

    Initializing an array of integers

---
+++ Topics/Array/Initializing an array of integers/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+/*
+Initializing an array of integers
+Initialize an int array named numbers with five elements 12, 17, 8, 101, 33 and output it.
+
+Use the provided code template.
+*/
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 17, 8, 101, 33};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
