3e77856590cda5084989dc6b17384d2582adae8b
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-12 17:00:02 +0200

    Creating a primitive

---
+++ Topics/Primitive and reference types/Creating a primitive/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /*
+    Creating a primitive
+        Create a variable of the primitive type, integer value.
+        Assign the JDK 1.0 release year as a value to the variable. Print the variable to the console.*/
+    public static void main(String[] args) {
+        int jdkReleaseYear = 1996;
+        System.out.println(jdkReleaseYear);
+    }
+}
