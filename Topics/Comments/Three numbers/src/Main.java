800b0818f08fbffaa7b0bf904babeb91cb2863db
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-12 17:24:30 +0200

    Three numbers

---
+++ Topics/Comments/Three numbers/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    /*
+    Three numbers
+        Given a program that prints numbers. Comment several lines so the program prints only three numbers:
+
+        5
+        6
+        7
+        */
+
+    public static void main(String[] args) {
+
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+//        System.out.println(8);
+    }
+}
