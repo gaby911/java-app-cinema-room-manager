3e434f56f184ee934666e7e406942ce04119b65d
Florin Abagiu <florin.abagiu91@gmail.com>
2023-02-16 19:15:25 +0200

    Rotate a rectangle array

--- Topics/Multidimensional array/Rotate a rectangle array/src/Main.java
+++ Topics/Multidimensional array/Rotate a rectangle array/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        int[][] input = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                input[i][j] = sc.nextInt();
+            }
+        }
+
+
+        int[][] output = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                output[j][rows - 1 - i] = input[i][j];
+            }
+        }
+
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(output[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
